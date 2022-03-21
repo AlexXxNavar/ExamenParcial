f578025c84bfc405b0975f6dff13230d4bb0b310
alexm <alexm@LAPTOP-KVL566MP>
2022-03-21 09:20:14 -0600

    Agregar archivos de proyecto.

---
+++ ExamenParcial/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿
+namespace ExamenParcial
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(186, 495);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 35);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Ver datos";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(186, 430);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 35);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Ingresar datos";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(108, 309);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(124, 20);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Ingrese el grado";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(112, 369);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(148, 26);
+            this.textBox1.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(103, 205);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(162, 20);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Seleccione una fecha";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(108, 95);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(167, 20);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Seleccione un Alumno";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(108, 261);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(298, 26);
+            this.dateTimePicker1.TabIndex = 9;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(108, 151);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(180, 28);
+            this.comboBox1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(483, 602);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ ExamenParcial/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExamenParcial
+{
+    public partial class Form1 : Form
+    {
+        List<Temperatura> temperaturas = new List<Temperatura>();
+        List<Departamento> departamentos = new List<Departamento>();
+        private void CargarDepartamentos(string file)
+        {
+            FileStream fs = new FileStream(file, FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+
+            while (sr.Peek() != -1)
+            {
+                Departamento departamento = new Departamento()
+                {
+                    id = Convert.ToInt32(sr.ReadLine()),
+                    nombre = sr.ReadLine(),
+                };
+                departamentos.Add(departamento);
+            }
+            sr.Close();
+            fs.Close();
+
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Cargar departamentos en combobox
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CargarDepartamentos("Departamentos.txt");
+            CargarTemperaturas("Temperaturas.txt");
+            foreach (Departamento departamento in departamentos)
+            {
+                comboBox1.Items.Add(departamento.nombre);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void CargarTemperaturas(string file)
+        {
+            FileStream fs = new FileStream(file, FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+            while (sr.Peek() != -1)
+            {
+                Temperatura temperatura = new Temperatura()
+                {
+                    id = Convert.ToInt32(sr.ReadLine()),
+                    magnitud = Convert.ToDouble(sr.ReadLine()),
+                    fecha = Convert.ToDateTime(sr.ReadLine())
+                };
+                temperaturas.Add(temperatura);
+            }
+            sr.Close();
+            fs.Close();
+        }
+        private void GuardarDatos(string file)
+        {
+            FileStream fs = new FileStream(file, FileMode.Create, FileAccess.ReadWrite);
+            StreamWriter sw = new StreamWriter(fs);
+            foreach (Temperatura temperatura in temperaturas)
+            {
+                sw.WriteLine(temperatura.id);
+                sw.WriteLine(temperatura.magnitud);
+                sw.WriteLine(temperatura.fecha);
+            }
+            sw.Close();
+            fs.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
